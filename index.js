6018d067ca450c5aee6f8024759db7a43969ba1d
Kristiyan Ivanov <kristiyan.ivanov.98@gmail.com>
2023-12-11 10:27:24 +0200

    Made update replace entry in database

--- index.js
+++ index.js
@@ -6,12 +6,12 @@ const Person = require('./models/person')
 
 const app = express()
 
-morgan.token('body', function(req, res) { return JSON.stringify(req.body) })
+morgan.token('body', function (req, res) { return JSON.stringify(req.body) })
 
+app.use(express.static('dist'))
 app.use(express.json())
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 app.use(cors())
-app.use(express.static('dist'))
 
 // Get all
 app.get('/api/people', (request, response) => {
@@ -26,21 +26,24 @@ app.get('/info', (request, response) => {
 // Get single
 app.get('/api/people/:id', (req, res) => {
     Person.findById(req.params.id)
-    .then(person => res.json(person))
-    .catch(error => console.log(`Person with id ${req.params.id} doesn't exist`, error))
+        .then(person => {
+            if (person) res.json(person)
+            else res.status(404).end()
+        })
+        .catch(error => next(error))
 })
 
 // Delete
 app.delete('/api/people/:id', (req, res) => {
-    // const id = Number(req.params.id)
-    // persons = persons.filter(p => p.id !== id)
-    // res.status(204).end()
+    Person.findByIdAndDelete(req.params.id)
+        .then(result => res.status(204).end())
+        .catch(error => next(error))
 })
 
 // Post
 app.post('/api/people', (req, res) => {
     const body = req.body
-    
+
     // if (!body.name || !body.number) {
     //     return res.status(400).json({
     //         error: 'name or number missing'
@@ -61,6 +64,36 @@ app.post('/api/people', (req, res) => {
         res.json(savedPerson)
     })
 })
+
+// Put
+app.put('/api/people/:id', (req, res) => {
+    const body = req.body
+
+    const person = {
+        name: body.name,
+        phone: body.phone,
+    }
+
+    Person.findByIdAndUpdate(req.params.id, person, { new: true })
+        .then(updatedPerson => res.json(updatedPerson))
+        .catch(error => next(error))
+})
+
+// Unknown endpoint
+const unknownEndpoint = (req, res) => {
+    res.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
+// Error handler
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+
+    if (error.name === 'CastError') return res.status(400).send({ error: 'malformatted id' })
+
+    next(error)
+}
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
